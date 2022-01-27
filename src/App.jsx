5a1320621441b34c738aa513d9ce813f5911f752
Patrick Machado <patrick@casadasfechaduras.br>
2022-01-27 13:29:24 -0300

    deletedContactPage

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Header from './components/partials/Header';
 import Footer from './components/partials/Footer';
 import About from './components/About';
-import Contact from './components/Contact';
 import Home from './components/Home';
 
 import './App.scss';
@@ -16,7 +15,6 @@ export default function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
-          <Route path="/contact" element={<Contact />} />
         </Routes>
         <Footer />
       </BrowserRouter>
